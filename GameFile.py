2219d96b2e4fd4a5fbfaf64d51203b30fa3d64cf
rmurry <rylanmurry@gmail.com>
2022-05-06 13:22:48 -0500

    re worked the character init to allow type selection as opposed to random rolls determining your character class

--- GameFile.py
+++ GameFile.py
@@ -1,36 +1,57 @@
 from random import randint
 import random
+import math
+import itertools
 
 class Character:
     def __init__(self):
+
         self.name = input("What is your name, Adventurer?")
-        self.health = randint(20,100)
-        if self.health < 40:
+
+        type_options = {1:"Tank",2:"Paladin",3:"Rogue",4:"Glass Cannon"}
+        
+        print("What class would you like to play as? This will determine your stats (type a number):\n1) Tank\n2) Paladin\n3) Rogue\n4) Glass Cannon")
+        type_choice = int(input())
+        while type_choice not in [1,2,3,4]:
+            print("Invalid Choice. What class would you like to play as? This will determine your stats (type a number):\n1) Tank\n2) Paladin\n3) Rogue\n4) Glass Cannon")
+            type_choice = int(input())
+        self.type = type_options[type_choice]
+
+        if self.type == 'Glass Cannon':
+            self.health =randint(20,40)
             self.attack = randint(15,20)
             self.speed = randint(5,10)
             self.ac = 3
-            self.type = 'Glass Cannon'
-        elif self.health < 60:
+            
+        elif self.type == 'Rogue':
+            self.health = randint(40,60)
             self.attack = randint(12,17)
             self.speed = randint(4,9)
             self.ac = 4
-            self.type = 'Rogue'
-        elif self.health < 80:
+
+        elif self.type == 'Paladin':
+            self.health = randint(60,80)
             self.attack = randint(9,14)
             self.speed = randint(3,8)
             self.ac = 5
-            self.type = 'Paladin'
-        elif self.health <= 100:
+
+        elif self.type == 'Tank':
+            self.health = randint(80,100)
             self.attack = randint(8,13)
             self.speed = randint(2,7)
             self.ac = 6
-            self.type = 'Tank'
+
         self.gold = 0
+
         self.level = 1
         self.xp = 0
 
+        level_req = {}
+        for i in range(1,10):
+            level_req[i] = (200 + (i * 50)) * (i-1)
+
     def Start(self):
-        print(f"You roll for stats.\nHealth is {self.health}\nAttack is {self.attack}\nSpeed is {self.speed}\nYou're playing as a {self.type}, which makes your Armor Level {self.ac}\nYou're Level {self.level} with {self.xp}xp\n")
+        print(f"You chose to play as a {self.type}. Your stats are:\nHealth: {self.health}\nAttack: {self.attack}\nSpeed: {self.speed}\nYour Armor Level is {self.ac}\nYou're Level {self.level} with {self.xp}xp\n")
 
     def earnGold(self,amount):
         self.gold += amount
@@ -54,6 +75,11 @@ class Character:
     def Attack(self):
         print(f"You attack for {self.attack}")
         return(self.attack)
+
+    def Level(self,amount):
+        self.xp += amount
+
+
 
 class Enemy:
     def __init__(self):
@@ -80,7 +106,7 @@ class Enemy:
             print(f"The {self.mon} has fainted!\n")
         else:
             self.health = self.health
-            print("The {self.mon} has {self.health} health left!\n")
+            print(f"The {self.mon} has {self.health} health left!\n")
 
 def Encounter():
     mon = Enemy()
@@ -116,14 +142,14 @@ hero.Start()
 
 value,mon = Encounter()
 
-if value == 1:
-    Fight(hero,mon)
-elif value == 2:
-    run_result = Run(hero,mon)
-    if run_result == 1:
-        print("game over\n") ## Placeholder for successful evasion
-    elif run_result == 2:
-        Fight(hero,mon)
+#if value == 1:
+#    Fight(hero,mon)
+#elif value == 2:
+#    run_result = Run(hero,mon)
+#    if run_result == 1:
+#        print("game over\n") ## Placeholder for successful evasion
+#    elif run_result == 2:
+#        Fight(hero,mon)
 
 
 
